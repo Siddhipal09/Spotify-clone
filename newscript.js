762eeb731d8ae57962fd83fd3b5b5d068e2bc5d0
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-31 01:53:31 +0530

    playslist section

--- newscript.js
+++ newscript.js
@@ -20,33 +20,41 @@
 
  }
  async function main() {
+    //get all the songs from the playlist
     let songs = await getSongs();
     console.log(songs);
+    //show all the songs in the playlist
     let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     
 
     for (const song of songs) {
-        songUL.innerHTML += `<li>${song.replaceAll("%20", " ")}</li>`;
+        songUL.innerHTML += `<li><img src="music.svg" alt="">
+        <div class="info">
+            <div> ${song.replaceAll("%20", " ")}</div>
+            <div>Siddhi</div>
+        </div>
+        <div class="playnow">
+            <span>Play Now</span>
+            <img src="play.svg" alt="">
+        </div></li>`;
     }
 
    
    
-    
-    if (songs.length > 0) {
-        let audioSrc = `http://127.0.0.1:5500/songs/${songs[0]}`;
-        console.log("Audio source:", audioSrc);
-
-        var audio = new Audio(audioSrc);
-       // var audio = new Audio(songs[0]);
-        audio.play();
-
-        audio.addEventListener("loadeddata", () => {
+    //play the first song
+    //    if (songs.length > 0) {
+    //      let audioSrc = `http://127.0.0.1:5500/songs/${songs[0]}`;
+    //      console.log("Audio source:", audioSrc);
+    //     var audio = new Audio(audioSrc);
+    //    // var audio = new Audio(songs[0]);
+    //    audio.play();
+    //     audio.addEventListener("loadeddata", () => {
             
-            console.log(audio.duration, audio.currentSrc, audio.currentTime);
-        });
-    } else {
-        console.error("No songs available.");
-    }
+    //      console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    //  });
+    // } else {
+    //     console.error("No songs available.");
+    // }
  }
 
 main();
